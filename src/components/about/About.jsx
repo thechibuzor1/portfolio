8fe6ced22331f766bb888496aa3c4b00c36baf5c
Chbuzor <49536616+thechibuzor1@users.noreply.github.com>
2022-07-18 16:34:13 +0100

    Updated about.js

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,18 +21,28 @@ const About = () => {
             <article className="about__card">
               <FaAward className="about__icon" />
               <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <small>1+ Years Working</small>
             </article>
             <article className="about__card">
               <FiUsers className="about__icon" />
               <h5>Clients</h5>
-              <small>300+ Worldwide</small>
+              <small>10+ Worldwide</small>
             </article>
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>80+ Completed</small>
+              <small>50+ Completed</small>
             </article>
+            <a
+              href="http://thechibuzorart.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+              <article className="art">
+                <img src={art} className="img" alt="my art" />
+                <h5>My Art hub</h5>
+              </article>
+            </a>
           </div>
           <p>
             As a problem solver who loves facing challenges in my work, I'm

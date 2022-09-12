d265b560f415cc7530613a7307396f09e2717d12
Chbuzor <49536616+thechibuzor1@users.noreply.github.com>
2022-09-12 15:29:03 +0100

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,59 +6,70 @@ import IMG3 from "../../assests/portfolio3.jpg";
 import IMG4 from "../../assests/portfolio4.jpg";
 import IMG5 from "../../assests/portfolio5.jpg";
 import IMG6 from "../../assests/netflix.png";
-import WeatherApp from "../../assests/Weather.apk";
-import UberEats from "../../assests/UberEats.apk";
-import Todo from "../../assests/Todo.apk";
+import IMG7 from "../../assests/netflixApp.jpg";
 
 const data = [
+  {
+    id: 1,
+    image: IMG6,
+    title: "Netflix Clone(Web)",
+    github: "https://github.com/thechibuzor1/netflix-clone",
+    demo: "http://netflix-remake.herokuapp.com",
+    description:
+      "A Full Stack Netflix Clone Written in ReactJS With MongoDb and nodejs backend.",
+  },
+  {
+    id: 2,
+    image: IMG7,
+    title: "Netflix Clone(APK)",
+    github: "https://github.com/thechibuzor1/Netflix-app-react-native",
+    demo: "https://mega.nz/file/QjRFCSaY#tP39KvSM3-VASNYPUCKEbzoImB9xJY0-zo5_n9NBwtw",
+    description:
+      "A Full Stack Netflix Clone Written in React Native With MongoDb and nodejs backend.",
+  },
+  {
+    id: 3,
+    image: IMG2,
+    title: "Project Gym",
+    github: "https://github.com/thechibuzor1/project_gym",
+    demo: "https://project-gym-app.herokuapp.com/",
+    description: "A Full Stack MERN E-commerce Gym Web App.",
+  },
+  {
+    id: 4,
+    image: IMG5,
+    title: "Uber Eats Clone",
+    github: "https://github.com/thechibuzor1/UberEats-v1.1",
+    demo: "https://mega.nz/file/c7xRQRqb#YboEM33nWS1pTa2mLCK3FLxWLltw3rCOcAECMxfSXfg",
+    description:
+      "A Restuarant E-commerce App Written in React Native With Firebase Database and authentication.",
+  },
+  {
+    id: 5,
+    image: IMG3,
+    title: "Chat App",
+    github: "https://github.com/thechibuzor1/chat-app",
+    demo: "https://project-chat-webapp.netlify.app/",
+    description:
+      "A Full Stack Realtime Chat Messaging App With Authentication & SMS Notification.",
+  },
   {
     id: 6,
     image: IMG1,
     title: "Weather App",
     github: "https://github.com/thechibuzor1/Weather-app",
-    demo: WeatherApp,
-    description: "A Simple UI Weather App Written In React Native."
+    demo: "https://mega.nz/file/Z25QDaiL#eTayeLHawh_BN6E6eW8ux3i--h1o5YN9NFByFjF99hY",
+    description: "A Simple UI Weather App Written In React Native.",
   },
+
   {
-    id: 2,
-    image: IMG2,
-    title: "Project Gym",
-    github: "https://github.com/thechibuzor1/project_gym",
-    demo: "https://project-gym-app.herokuapp.com/",
-    description: "A Full Stack MERN E-commerce Gym Web App."
-  },
-  {
-    id: 4,
-    image: IMG3,
-    title: "Chat App",
-    github: "https://github.com/thechibuzor1/chat-app",
-    demo: "https://project-chat-webapp.netlify.app/",
-    description: "A Full Stack Realtime Chat Messaging App With Authentication & SMS Notification."
-  },
-  {
-    id: 5,
+    id: 7,
     image: IMG4,
     title: "Todo App",
     github: "https://github.com/thechibuzor1/Todo-list",
-    demo: Todo,
-    description: "A simple UI Todo App Written In React Native With Async Storage."
-  },
-  {
-    id: 3,
-    image: IMG5,
-    title: "Uber Eats Clone",
-    github: "https://github.com/thechibuzor1/UberEats-v1.1",
-    demo: UberEats,
-    description: "A Restuarant E-commerce App With Firebase Written in React Native."
-  },
-   {
-    id: 1,
-    image: IMG6,
-    title: "Netflix Clone",
-    github: "https://github.com/thechibuzor1/netflix-clone",
-    demo: "http://netflix-remake.herokuapp.com",
+    demo: "https://mega.nz/file/ByQjST5Z#kpreAj0QBEE0M_jhFNTr4ente-p8TYjwGeoHCurJdAQ",
     description:
-      "A Full Stack Netflix Clone Written in React With MongoDb and nodejs backend.",
+      "A simple UI Todo App Written In React Native With Async Storage.",
   },
 ];
 

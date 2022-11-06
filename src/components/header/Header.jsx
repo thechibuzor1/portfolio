77a476b62698392a2dda384062839da6bc5150bb
Chbuzor <49536616+thechibuzor1@users.noreply.github.com>
2022-11-06 19:42:31 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,10 +13,6 @@ const Header = () => {
         <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <HeaderSocials />
-
-        <div className="me">
-          <img src={ME} alt="me"/>
-        </div>
         <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
     </header>

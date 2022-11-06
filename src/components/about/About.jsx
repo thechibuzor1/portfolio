589ce82bf52b4d4d452900b449fc2a5f256dbf42
Chbuzor <49536616+thechibuzor1@users.noreply.github.com>
2022-11-06 19:43:03 +0100

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,11 +12,6 @@ const About = () => {
       <h5>Get To Know</h5>
       <h2>About Me</h2>
       <div className="container about__container">
-    <div className="about__me">
-          <div className="about__me-image">
-            <img src={ME} alt="me" />
-          </div>
-        </div>
         <div className="about__content">
           <div className="about__cards">
             <article className="about__card">

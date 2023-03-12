5a8082b57c1d07c1fddcfe06a8f3184d5ed3b1d4
Chibuzor <chibuzorm26@example.com>
2023-03-12 22:07:22 +0100

    updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import CTA from "./CTA";
-import ME from "../../assests/me.png";
+import TypeWriterEffect from "react-typewriter-effect";
 import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
@@ -9,11 +9,32 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Chibuzor Moses</h1>
+        <TypeWriterEffect
+          textStyle={{
+            fontWeight: 500,
+            fontSize: "2.5rem",
+            color: "#ffffff",
+            textAlign: "center",
+          }}
+          startDelay={2000}
+          cursorColor="#ffffff"
+          multiText={[
+            "Chibuzor Moses",
+            "6ft 2...",
+            "Specialized in React and React Native",
+            "Creative",
+            "Can bench 225lb...",
+            "Chibuzor Moses",
+          ]}
+          multiTextDelay={1000}
+          typeSpeed={100}
+        />
         <h5 className="text-light">Full Stack Developer</h5>
         <CTA />
         <HeaderSocials />
-        <a href="#contact" className="scroll__down">Scroll Down</a>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
       </div>
     </header>
   );
